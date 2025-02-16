06ad7230ef949cd6e701c62dcf390eb39fc0575e
Bang210 <bangvelope@gmail.com>
2025-02-16 17:15:09 +0900

    init

---
+++ src/main/java/kdd/toy/easynaramarket/EasyNaraMarketApplication.java
@@ -0,0 +1,13 @@
+package kdd.toy.easynaramarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNaraMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyNaraMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kdd/toy/easynaramarket/EasyNaraMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kdd.toy.easynaramarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNaraMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
