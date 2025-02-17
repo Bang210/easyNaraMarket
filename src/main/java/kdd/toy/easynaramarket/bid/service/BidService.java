f228892c64fe2cd002c3c52def1fd4c9d19264d9
Bang210 <bangvelope@gmail.com>
2025-02-17 16:13:21 +0900

    feat: create webClient

--- src/main/java/kdd/toy/easynaramarket/bid/service/BidService.java
+++ src/main/java/kdd/toy/easynaramarket/bid/service/BidService.java
@@ -2,8 +2,12 @@ package kdd.toy.easynaramarket.bid.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 public class BidService {
+
+    private final WebClient webClient;
+
 }
---
+++ src/main/java/kdd/toy/easynaramarket/global/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package kdd.toy.easynaramarket.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl("http://apis.data.go.kr/1230000/ad/BidPublicInfoService")
+                .build();
+    }
+}
