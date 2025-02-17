f118cff70f3ed5a728d27ddbde3c71c93985fc0a
Bang210 <bangvelope@gmail.com>
2025-02-17 14:50:46 +0900

    feat: create controller, service

--- src/main/java/kdd/toy/easynaramarket/bid/controller/BidController.java
+++ src/main/java/kdd/toy/easynaramarket/bid/controller/BidController.java
@@ -1,5 +1,20 @@
 package kdd.toy.easynaramarket.bid.controller;
 
+import kdd.toy.easynaramarket.bid.service.BidService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/bid")
 public class BidController {
+
+    private final BidService bidService;
+
+    @GetMapping("/getBidList")
+    public String getBidList() {
+        return "bidList";
+    }
 }
